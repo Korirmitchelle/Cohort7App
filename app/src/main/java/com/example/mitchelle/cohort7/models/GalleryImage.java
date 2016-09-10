0c9905b6e9c7997b6bb42fc1c36b0e10cce14500
Korirmitchelle <korirmitchelle@gmail.com>
2016-09-10 17:34:54 +0300

    Retrofit api interface for Gallery Fragment

--- app/src/main/java/com/example/mitchelle/cohort7/models/GalleryImage.java
+++ app/src/main/java/com/example/mitchelle/cohort7/models/GalleryImage.java
@@ -4,4 +4,41 @@ package com.example.mitchelle.cohort7.models;
  * Created by mitchelle on 9/10/16.
  */
 public class GalleryImage {
+    private String thumbnail;
+    private String image;
+    private String caption;
+
+    public GalleryImage() {
+
+    }
+
+    public GalleryImage( String thumbnail, String image, String caption ) {
+        this.thumbnail = thumbnail;
+        this.image = image;
+        this.caption = caption;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public void setThumbnail(String thumbnail) {
+        this.thumbnail = thumbnail;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCaption() {
+        return caption;
+    }
+
+    public void setCaption(String caption) {
+        this.caption = caption;
+    }
 }
---
+++ app/src/main/java/com/example/mitchelle/cohort7/utils/GalleryApi.java
@@ -0,0 +1,7 @@
+package com.example.mitchelle.cohort7.utils;
+
+/**
+ * Created by mitchelle on 9/10/16.
+ */
+public interface GalleryApi {
+}
