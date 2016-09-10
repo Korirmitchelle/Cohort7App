ef80121a8f75650cee5e8bdafd7d8761d3aa55d5
Korirmitchelle <korirmitchelle@gmail.com>
2016-09-10 13:45:12 +0300

    layout for ExhibitsAbout

--- app/src/main/java/com/example/mitchelle/cohort7/activities/ExhibitsAbout.java
+++ app/src/main/java/com/example/mitchelle/cohort7/activities/ExhibitsAbout.java
@@ -1,7 +1,17 @@
 package com.example.mitchelle.cohort7.activities;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by mitchelle on 9/10/16.
  */
-public class ExhibitsAbout {
+public class ExhibitsAbout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
